a9c1011af13e430d794bd5c84c6e46a07aae6196
Eiad Soufan <eiad.soufan.2@gmail.com>
2025-12-19 14:21:08 +0800

    Update settings.py

--- epicblog_api/settings.py
+++ epicblog_api/settings.py
@@ -149,9 +149,12 @@ SIMPLE_JWT = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
